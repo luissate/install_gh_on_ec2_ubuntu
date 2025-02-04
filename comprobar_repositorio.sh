fca8a4c1c313d1d9c768cc7f6edbb53dbb410119
luissate <91691523+luissate@users.noreply.github.com>
2025-02-04 19:35:34 +0100

    Update comprobar_repositorio.sh

--- comprobar_repositorio.sh
+++ comprobar_repositorio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-check_gh_installed() {
+comprobar_instalacion_gh() {
     if command -v gh &> /dev/null; then
         return 1  # gh está instalado
     else
@@ -8,7 +8,15 @@ check_gh_installed() {
     fi
 }
 
-check_gh_installed
+comprobar_instalacion_gh
+
+if [ $? -eq 1 ]; then
+    echo "El programa gh ya está instalado"
+else
+    echo "El programa gh no está instalado"
+fi
+
 exit $?  # Devuelve el código de salida de la función
+
 
 
